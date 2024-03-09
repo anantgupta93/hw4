c9c78fc05e438a3127afd5c0625c19f94671efd2
anantgupta93 <anant.gupta@kellogg.northwestern.edu>
2024-03-09 00:58:45 +0000

    hw4

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,45 @@
 class PlacesController < ApplicationController
-
+  before_action :authenticate_user, except: [:index, :show]
+  before_action :set_place, only: [:show]
+before_action :set_user, only: [:create, :new]
   def index
     @places = Place.all
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @entries = @place.entries
   end
 
   def new
+    @place = Place.new
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    @place = @user.places.build(place_params)
+    
+    if @place.save
+      redirect_to places_path, notice: 'Place was successfully created.'
+    else
+      render :new
+    end
   end
 
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :location)
+  end
+
+  def set_user
+    @user = User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate_user
+    set_user
+    redirect_to login_path, alert: 'You must be logged in to access this section.' unless @user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
-
+  
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
+    @user["username"] = params["username"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
-    redirect_to "/"
+    flash[:notice] = "Thanks for signing up. Now login."
+    redirect_to "/login"
   end
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
 end
